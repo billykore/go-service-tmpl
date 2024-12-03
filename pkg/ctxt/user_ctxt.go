9fd7e2e7c52eec3f873be4e0fbba8335c81f5f6b
Billy Kore <evanbill.kore@bankraya.co.id>
2024-12-03 13:47:04 +0700

    Add project packages

---
+++ pkg/ctxt/user_ctxt.go
@@ -0,0 +1,23 @@
+package ctxt
+
+import (
+	"context"
+	"errors"
+
+	"github.com/billykore/kore/backend/pkg/entity"
+)
+
+var ErrUserFromContext = errors.New("failed to get user from context")
+
+const UserContextKey = "user"
+
+// ContextWithUser set user data to the ctx context.
+func ContextWithUser(ctx context.Context, user entity.User) context.Context {
+	return context.WithValue(ctx, UserContextKey, user)
+}
+
+// UserFromContext gets user data from ctx context.
+func UserFromContext(ctx context.Context) (entity.User, bool) {
+	user, ok := ctx.Value(UserContextKey).(entity.User)
+	return user, ok
+}
---
+++ pkg/db/postgres/connection.go
@@ -0,0 +1,20 @@
+package postgres
+
+import (
+	"github.com/billykore/kore/backend/pkg/config"
+	"github.com/billykore/kore/backend/pkg/logger"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// New returns new postgres db connection.
+func New(cfg *config.Config) *gorm.DB {
+	dsn := cfg.Postgres.DSN
+	log := logger.New()
+	db, err := gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		log.Usecase("New").Fatalf("failed to connect database: %v", err)
+		return nil
+	}
+	return db
+}
